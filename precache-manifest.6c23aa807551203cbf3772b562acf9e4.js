fcb93877c8dd2d2845b578073a1d1b5c30ce3647
Aadi <aadiyaara@gmail.com>
2020-04-05 20:34:54 +0530

    init test

---
+++ precache-manifest.6c23aa807551203cbf3772b562acf9e4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86c9f6a88532cd886050bfc66d5613e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7042e8431b410078501e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ff60c99ff348e0ddb24",
+    "url": "/static/js/3.a005a8ae.chunk.js"
+  },
+  {
+    "revision": "09ffef4336c7429f8dd5",
+    "url": "/static/js/4.d271b533.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/4.d271b533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "428c5c8ae91df105eda1",
+    "url": "/static/js/5.e7fd288f.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/5.e7fd288f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c852a858abf594ab6a5",
+    "url": "/static/js/6.b099c6fb.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/static/js/6.b099c6fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7042e8431b410078501e",
+    "url": "/static/js/main.418f0b29.chunk.js"
+  },
+  {
+    "revision": "949bd98e42d9e8588afe",
+    "url": "/static/js/runtime-main.7b6feae9.js"
+  },
+  {
+    "revision": "38a7b86e635a9582edf4",
+    "url": "/static/js/xlsx.a493b133.chunk.js"
+  }
+]);
